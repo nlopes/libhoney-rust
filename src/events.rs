8f14d9398dcfaa7f8e9d3790a091e4891bdf0565
Norberto Lopes <nlopes.ml@gmail.com>
2019-09-07 19:03:53 +0100

    Move Events to its own file (events.rs)

---
+++ src/events.rs
@@ -0,0 +1,45 @@
+use std::time::{Duration, Instant};
+
+use reqwest::StatusCode;
+
+use crate::event::Event;
+use crate::response::Response;
+
+pub(crate) type Events = Vec<Event>;
+
+pub(crate) trait EventsResponse {
+    fn to_response(
+        &self,
+        status: Option<StatusCode>,
+        body: Option<String>,
+        clock: Instant,
+        error: Option<String>,
+    ) -> Vec<Response>;
+}
+
+impl EventsResponse for Events {
+    fn to_response(
+        &self,
+        status: Option<StatusCode>,
+        body: Option<String>,
+        clock: Instant,
+        error: Option<String>,
+    ) -> Vec<Response> {
+        let total_events = if self.is_empty() {
+            1
+        } else {
+            self.len() as u64
+        };
+
+        let spent = Duration::from_secs(clock.elapsed().as_secs() / total_events);
+        self.iter()
+            .map(|ev| Response {
+                status_code: status,
+                body: body.clone(),
+                duration: spent,
+                metadata: ev.metadata.clone(),
+                error: error.clone(),
+            })
+            .collect()
+    }
+}
--- src/transmission.rs
+++ src/transmission.rs
@@ -18,47 +18,9 @@ use tokio_timer::clock::Clock;
 use crate::errors::{Error, Result};
 use crate::event::Event;
 use crate::eventdata::EventData;
+use crate::events::{Events, EventsResponse};
 use crate::response::{HoneyResponse, Response};
 use crate::sender::Sender;
-
-type Events = Vec<Event>;
-
-trait EventsResponse {
-    fn to_response(
-        &self,
-        status: Option<StatusCode>,
-        body: Option<String>,
-        clock: Instant,
-        error: Option<String>,
-    ) -> Vec<Response>;
-}
-
-impl EventsResponse for Events {
-    fn to_response(
-        &self,
-        status: Option<StatusCode>,
-        body: Option<String>,
-        clock: Instant,
-        error: Option<String>,
-    ) -> Vec<Response> {
-        let total_events = if self.is_empty() {
-            1
-        } else {
-            self.len() as u64
-        };
-
-        let spent = Duration::from_secs(clock.elapsed().as_secs() / total_events);
-        self.iter()
-            .map(|ev| Response {
-                status_code: status,
-                body: body.clone(),
-                duration: spent,
-                metadata: ev.metadata.clone(),
-                error: error.clone(),
-            })
-            .collect()
-    }
-}
 
 const BATCH_ENDPOINT: &str = "/1/batch/";
 
